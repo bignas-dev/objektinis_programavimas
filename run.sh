5fd95a0b9f424a0be5dc6f25a34f343759276ff2
bignas <redfisher69@protonmail.com>
2026-02-15 18:31:20 +0200

    istrinti seni *.cpp failai bei igalinimas bash programos sukompiliuoti ir paleisti bet kuri is sukurtu cpp failu

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-g++ main.cpp -Wall -Wextra -o exec && ./exec
+#!/bin/bash
+
+if [ "$1" == "1" ]; then
+    g++ c_array.cpp -Wall -Wextra -o exec && ./exec
+    exit 0
+fi
+
+if [ "$1" == "2" ]; then
+    g++ vector.cpp -Wall -Wextra -o exec && ./exec
+    exit 0
+fi
+
+echo "Invalid argument: wanted '1' or '2', got '$1' (blank == not supplied)"
+exit 67
+
