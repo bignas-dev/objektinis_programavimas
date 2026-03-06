abeff5b0d31b8af00f9e700a717daad0803e272b
bignas <redfisher69@protonmail.com>
2026-03-06 16:42:49 +0200

    Sukurtas utils.h su bendromis vardų ir pavardžių deklaracijomis

---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+extern const std::string vardai[];
+extern const std::string pavardes[];
+
+int getVarduKiekis();
+int getPavardziuKiekis();
+
+#endif
