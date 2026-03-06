fbfaa35b12fa2bf92a30118e82c5ffa93b196a71
bignas <redfisher69@protonmail.com>
2026-03-06 14:59:25 +0200

    1 isimtis suvaldyta

--- c_array.cpp
+++ c_array.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <stdexcept>
 
 struct Mokinys {
     std::string vardas;
@@ -66,15 +67,19 @@ void readStudentData(Mokinys& mokinys) {
         int grade;
         std::cin >> grade;
 
-        if (std::cin.fail()) {
-            std::cout << "Neteisinga įvestis. Bandykite dar kartą.\n";
-            continue;
-        }
-        if (grade == -1) {
-            break;
-        }
-        if (grade < 0 || grade > 10) {
-            std::cout << "Rezultatas turi būti nuo 0 iki 10. Bandykite dar kartą.\n";
+        try {
+            std::cin >> grade;
+            if (std::cin.fail()) {
+                throw std::runtime_error("Neteisinga įvestis. Bandykite dar kartą.\n");
+            }
+            if (grade == -1) {
+                break;
+            }
+            if (grade < 0 || grade > 10) {
+                throw std::runtime_error("Rezultatas turi būti nuo 0 iki 10. Bandykite dar kartą.\n");
+            }
+        } catch (const std::runtime_error& e) {
+            std::cout << e.what();
             continue;
         }
         if (count == capacity) {
