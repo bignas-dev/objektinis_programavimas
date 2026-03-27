461c6901ae9143c6340419ffccfd66c4f148ec77
anon <anon@nixos.balls>
2026-03-27 17:29:35 +0200

    Atnaujintas strategijų aprašymas

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     
     std::cout << "Pradedami v1.0 spartos tyrimai...\n";
     std::cout << "Tiriami konteineriai: vector, list, deque\n";
-    std::cout << "Strategijos: 1 (du nauji), 2 (vienas naujas + trynimas), 3 (partition)\n\n";
+    std::cout << "Strategijos: 1 (du nauji konteineriai), 2 (vienas naujas + trynimas), 3 (std::partition)\n\n";
     
     std::vector<BenchmarkResult> allResults;
     
